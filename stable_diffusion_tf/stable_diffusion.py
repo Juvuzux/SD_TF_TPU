1c2c6feb3df29691a334d0629547fa817017c927
Juvuzux <82806822+Juvuzux@users.noreply.github.com>
2022-09-21 19:11:17 +1000

    Update stable_diffusion.py

--- stable_diffusion_tf/stable_diffusion.py
+++ stable_diffusion_tf/stable_diffusion.py
@@ -159,21 +159,27 @@ def get_models(img_height, img_width, download_weights=True):
     decoder = keras.models.Model(latent, decoder(latent))
 
     text_encoder_weights_fpath = keras.utils.get_file(
-        fname="text_encoder.h5",
+        "/kaggle/input/kerasdiffusionmodels/text_encoder.h5",
+        origin="",
+        #fname="text_encoder.h5",
         #origin="/kaggle/input/kerasdiffusionmodels/text_encoder.h5",
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/text_encoder.h5",
+        #origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/text_encoder.h5",
         #file_hash="d7805118aeb156fc1d39e38a9a082b05501e2af8c8fbdc1753c9cb85212d6619",
     )
     diffusion_model_weights_fpath = keras.utils.get_file(
-        fname="diffusion_model.h5",
+        "/kaggle/input/kerasdiffusionmodels/diffusion_model.h5",
+        origin="",
+        #fname="diffusion_model.h5",
         #origin="/kaggle/input/kerasdiffusionmodels/diffusion_model.h5",
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/diffusion_model.h5",
+        #origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/diffusion_model.h5",
         #file_hash="a5b2eea58365b18b40caee689a2e5d00f4c31dbcb4e1d58a9cf1071f55bbbd3a",
     )
     decoder_weights_fpath = keras.utils.get_file(
-        fname="decoder.h5",
+        "/kaggle/input/kerasdiffusionmodels/decoder.h5",
+        origin="",
+        #fname="decoder.h5",
         #origin="/kaggle/input/kerasdiffusionmodels/decoder.h5",
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/decoder.h5",
+        #origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/decoder.h5",
         #file_hash="6d3c5ba91d5cc2b134da881aaa157b2d2adc648e5625560e3ed199561d0e39d5",
     )
 
